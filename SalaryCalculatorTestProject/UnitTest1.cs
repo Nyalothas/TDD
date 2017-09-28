993480a8bb532fe345ce1739b17f14ce713392d6
Ny'alotha <Nyalotha@outlook.com>
2017-09-28 15:35:11 +0300

    initial commit

--- SalaryCalculatorTestProject/UnitTest1.cs
+++ SalaryCalculatorTestProject/UnitTest1.cs
@@ -3,6 +3,14 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SalaryCalculatorTestProject
 {
+    /*		
+     To get hourly, divide annual salary by 2080     
+     $100,006.40 / 2080 = $48.08  hr
+
+     To get annual, multiply hourly by 2080 
+     $48.08 * 2080 = $100,006.40
+     */
+
     [TestClass]
     public class UnitTest1
     {

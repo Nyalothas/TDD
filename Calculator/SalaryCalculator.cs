bb2d153a73256f614f45242d043c129bf65453cf
Ny'alotha <Nyalotha@outlook.com>
2017-09-28 16:04:31 +0300

    refactored

--- Calculator/SalaryCalculator.cs
+++ Calculator/SalaryCalculator.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calculator
+﻿namespace Calculator
 {
     public class SalaryCalculator
     {
+        const int HoursInYear = 2080;
         public decimal GetAnnualSalary(decimal hourlyWage)
         {
-            const int HoursInYear = 2080;
-            decimal annualSalary = hourlyWage * HoursInYear;
-            return annualSalary;
+            return hourlyWage * HoursInYear;
         }
     }
 }

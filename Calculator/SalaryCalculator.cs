7bc6a7542d15b35192b807ee89eeb539f15ac9c7
Ny'alotha <Nyalotha@outlook.com>
2017-09-28 16:15:15 +0300

    refactored using expression body function members

--- Calculator/SalaryCalculator.cs
+++ Calculator/SalaryCalculator.cs
@@ -3,14 +3,8 @@
     public class SalaryCalculator
     {
         const int HoursInYear = 2080;
-        public decimal GetAnnualSalary(decimal hourlyWage)
-        {
-            return hourlyWage * HoursInYear;
-        }
+        public decimal GetAnnualSalary(decimal hourlyWage) => hourlyWage * HoursInYear;
 
-        public decimal GetHourlyWage(int annualSalary)
-        {
-            return annualSalary/HoursInYear;
-        }
+        public decimal GetHourlyWage(int annualSalary) => annualSalary/HoursInYear;
     }
 }
